ce6a1269ee400f48ee788a07f264afa44bb1527f
alpacawool <44758340+alpacawool@users.noreply.github.com>
2021-04-08 22:04:23 -0700

    Added base project files "Hello World"

---
+++ index.js
@@ -0,0 +1,33 @@
+/* Beaverhacks Spring 2021
+** Paper Trail
+** Team Members: Derth Adams, Kelton Orth, 
+**               Yongsung Cho, Patricia Booth
+** Instructions: To run locally (node.js required):
+    * Navigate to root project directory on terminal
+    * Type the following lines:
+    *   npm install
+    *   node index.js
+    * Visit site on http://localhost:5000/
+*/
+
+const express = require('express');
+const port = 5000;
+
+// Initialize express and set view engine to handlebars(.hbs)
+const app = express();
+const handlebars = require('express-handlebars');
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+    extname: 'hbs'
+}));
+
+app.use(express.static('public'));
+
+// Loads main.hbs inside index.hbs
+app.get('/', (req, res) => {
+    res.render('main', {layout: 'index'})
+});
+
+app.listen(port, () => 
+    console.log(`Express running on port ${port}`));
